4798dd6768cfd54e241bf16e639d1a3683850ebe
Jordan Marr <jmarr@microdesk.com>
2019-04-20 04:01:47 -0400

    Updated .addin file.

--- src/RevitFabulous.WPF/CounterPage.fs
+++ src/RevitFabulous.WPF/CounterPage.fs
@@ -42,17 +42,19 @@ module CounterPage =
 
     let view (model: Model) dispatch =
         View.ContentPage(
-          content = View.StackLayout(padding = 20.0, verticalOptions = LayoutOptions.Center,
-            children = [ 
-                View.Label(text = sprintf "%d" model.Count, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
-                View.Button(text = "Increment (+)", command = (fun () -> dispatch Increment), horizontalOptions = LayoutOptions.Center, widthRequest = 100.)
-                View.Button(text = "Decrement (-)", command = (fun () -> dispatch Decrement), horizontalOptions = LayoutOptions.Center)
-                View.Label(text = "Timer", horizontalOptions = LayoutOptions.Center)
-                View.Switch(isToggled = model.TimerOn, toggled = (fun on -> dispatch (TimerToggled on.Value)), horizontalOptions = LayoutOptions.Center)
-                View.Slider(minimumMaximum = (0.0, 10.0), value = double model.Step, valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))), horizontalOptions = LayoutOptions.FillAndExpand)
-                View.Label(text = sprintf "Step size: %d" model.Step, horizontalOptions = LayoutOptions.Center) 
-                View.Button(text = "Reset", horizontalOptions = LayoutOptions.Center, command = (fun () -> dispatch Reset), canExecute = (model <> initModel))
-            ]))
+            View.StackLayout(
+                padding = 20.0, 
+                verticalOptions = LayoutOptions.Center,
+                children = [ 
+                    View.Label(text = sprintf "%d" model.Count, horizontalOptions = LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
+                    View.Button(text = "Increment (+)", command = (fun () -> dispatch Increment), horizontalOptions = LayoutOptions.Center, widthRequest = 100.)
+                    View.Button(text = "Decrement (-)", command = (fun () -> dispatch Decrement), horizontalOptions = LayoutOptions.Center)
+                    View.Label(text = "Timer", horizontalOptions = LayoutOptions.Center)
+                    View.Switch(isToggled = model.TimerOn, toggled = (fun on -> dispatch (TimerToggled on.Value)), horizontalOptions = LayoutOptions.Center)
+                    View.Slider(minimumMaximum = (0.0, 10.0), value = double model.Step, valueChanged = (fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))), horizontalOptions = LayoutOptions.FillAndExpand)
+                    View.Label(text = sprintf "Step size: %d" model.Step, horizontalOptions = LayoutOptions.Center) 
+                    View.Button(text = "Reset", horizontalOptions = LayoutOptions.Center, command = (fun () -> dispatch Reset), canExecute = (model <> initModel))
+                ]))
 
     // Note, this declaration is needed if you enable LiveUpdate
     let program = Program.mkProgram init update view
