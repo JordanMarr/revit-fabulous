bf2b65d1e99e7ebc4df545f0e0e82c77007083e6
Jordan Marr <jmarr@microdesk.com>
2019-04-20 04:02:43 -0400

    Removed commented code

--- src/RevitFabulous.WPF/Main.fs
+++ src/RevitFabulous.WPF/Main.fs
@@ -3,7 +3,6 @@
 /// Used for testing UI without Revit as a standalone app.
 module Main = 
     open System
-    open Xamarin.Forms
 
     [<EntryPoint>]
     [<STAThread>]
@@ -11,14 +10,3 @@ module Main =
 
         Controller.showCounterDialog()
         0
-
-        //let app = new System.Windows.Application()
-        //Forms.Init()
-        //let window = MainWindow() 
-        
-        //CounterPage.program 
-        //|> GenericApp
-        //|> window.LoadApplication
-
-        //app.Run(window)
-
